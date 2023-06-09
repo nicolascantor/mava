4612d45560c84184b7ce7c76b407cf8f03dbf792
Nicolas Cantor <nicolascantor103@gmail.com>
2023-06-09 16:14:49 -0500

    first commit

---
+++ database/migrations/2023_06_09_192145_create_sedes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sedes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('direccion');
+            $table->string('telefono');
+            $table->string('ciudad');
+            $table->unsignedBigInteger('regimen_simplificado_id');
+            $table->foreign('regimen_simplificado_id')->references('id')->on('simplified_regimens');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sedes');
+    }
+};
