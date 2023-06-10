ba47938ed7fa606897c229a119a2d32c211b8e37
unknown <nicolascantor103@gmail.com>
2023-06-10 16:47:13 -0500

    formulario usuario

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,12 +4,42 @@
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+            <x-input-label for="nombre" :value="__('Nombre')" />
+            <x-text-input id="nombre" class="block mt-1 w-full" type="text" name="nombre" :value="old('nombre')" required autofocus autocomplete="nombre" />
+            <x-input-error :messages="$errors->get('nombre')" class="mt-2" />
+        </div>
+
+        <!-- Apellido -->
+        <div>
+            <x-input-label for="apellido" :value="__('Apellido')" />
+            <x-text-input id="apellido" class="block mt-1 w-full" type="text" name="apellido" :value="old('apellido')" required autofocus autocomplete="apellido" />
+            <x-input-error :messages="$errors->get('apellido')" class="mt-2" />
+        </div>
+
+        <!-- Celular -->
+        <div>
+            <x-input-label for="celular" :value="__('Celular')" />
+            <x-text-input id="celular" class="block mt-1 w-full" type="text" name="celular" :value="old('celular')" required autofocus autocomplete="celular" />
+            <x-input-error :messages="$errors->get('celular')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
+        <div class="mt-4">
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" >
+                {{ '' }}
+            </x-text-input>
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- Tipo Documento -->
+        <div class="mt-4">
+            <x-input-label for="tipo_documento" :value="__('Tipo de documento')" />
+            <x-select-input id="tipo_documento" class="block mt-1 w-full" type="text" name="tipo_documento" :value="old('tipo_documento')" required autocomplete="tipo_documento" />
+            <x-input-error :messages="$errors->get('tipo_documento')" class="mt-2" />
+        </div>
+
+        <!-- Numero Documento -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
